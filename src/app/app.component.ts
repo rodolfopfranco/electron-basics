be5c4cf86382b24a4dcc72b025dc60509138439a
rodolfopfranco <rodolfofrancolemos@hotmail.com>
2023-01-11 21:50:39 -0300

    timer configs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { interval } from 'rxjs';
+import { takeWhile, tap } from 'rxjs/operators'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,35 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app-teste';
+
+  max = 1;
+  current =0;
+
+  start() {
+    const intervalo = interval(100);
+
+    intervalo.pipe(takeWhile(_ => !this.isFinished),
+    tap( i => this.current+ 0.1))
+    .subscribe();
+  }
+
+  finish() {
+    this.current = this.max;
+  }
+
+  reset() {
+    this.current = 0;
+  }
+
+  get maxVal(){
+    return isNaN(this.max) || this.max < 0.1 ? 0.1 : this.max;
+  }
+
+  get currentVal() {
+    return isNaN(this.current) || this.current < 0 ? 0 : this.current;
+  }
+
+  get isFinished() {
+    return this.currentVal >= this.max;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
+import { RoundProgressModule } from 'angular-svg-round-progressbar';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    RoundProgressModule
   ],
   providers: [],
   bootstrap: [AppComponent]
